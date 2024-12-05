3c24ad732e64815fa0db4270bc09b8b6f51e57ec
AndreySanzharov <Andrsan2004@yandex.ru>
2024-12-05 13:52:39 +0300

    Поля для класса бронирования и запросов

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,8 +1,19 @@
 package ru.practicum.shareit.booking.dto;
 
+import ru.practicum.shareit.booking.Status;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+
 /**
  * TODO Sprint create-bookings.
  */
 public class BookingDto {
-
+    private Integer id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private Item item;
+    private User booker;
+    private Status status;
 }
