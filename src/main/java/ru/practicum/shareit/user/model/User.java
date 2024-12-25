9e4e4ed98a9271bd4be1d8222ae8460e8f8e582f
AndreySanzharov <Andrsan2004@yandex.ru>
2024-12-25 17:23:08 +0300

    Сделаны пользователи

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,13 +1,34 @@
 package ru.practicum.shareit.user.model;
 
-
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
+
+@Entity
 @Data
+@Table(name = "users")
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @Positive
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @NotNull
     private String name;
+
+    @Column(name = "email", length = 50, nullable = false, unique = true)
+    @NotNull
+    @Email
     private String email;
 }
